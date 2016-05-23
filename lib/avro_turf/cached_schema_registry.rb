612b46c0b2f7c1de300bfabb6104c78497c093fb
david-richey <david.richey@validic.com>
2016-05-23 12:49:15 -0400

    register choice, save schema registry to cache

--- lib/avro_turf/cached_schema_registry.rb
+++ lib/avro_turf/cached_schema_registry.rb
@@ -3,10 +3,11 @@ require 'avro_turf/schema_registry'
 # Caches registrations and lookups to the schema registry in memory.
 class AvroTurf::CachedSchemaRegistry
 
-  def initialize(upstream)
+  def initialize(upstream, register: true)
     @upstream = upstream
     @schemas_by_id = {}
     @ids_by_schema = {}
+    @register = register
   end
 
   # Delegate the following methods to the upstream
@@ -21,6 +22,15 @@ class AvroTurf::CachedSchemaRegistry
   end
 
   def register(subject, schema)
-    @ids_by_schema[subject + schema.to_s] ||= @upstream.register(subject, schema)
+    @ids_by_schema[subject + schema.to_s] ||= upstream_register(subject, schema)
+  end
+
+  def upstream_register(subject, schema)
+    @upstream.register(subject, schema) if @register
+  end
+
+  def store!(subject, schema, id)
+    @schemas_by_id[id] = schema.to_s
+    @ids_by_schema[subject + schema.to_s] = id
   end
 end
--- lib/avro_turf/messaging.rb
+++ lib/avro_turf/messaging.rb
@@ -16,18 +16,18 @@ class AvroTurf
   # 1: https://github.com/confluentinc/schema-registry
   class Messaging
     MAGIC_BYTE = [0].pack("C").freeze
-
     # Instantiate a new Messaging instance with the given configuration.
     #
     # registry_url - The String URL of the schema registry that should be used.
     # schemas_path - The String file system path where local schemas are stored.
     # namespace    - The String default schema namespace.
     # logger       - The Logger that should be used to log information (optional).
-    def initialize(registry_url: nil, schemas_path: nil, namespace: nil, logger: nil)
+    # register     - The Boolean for wheather or not to register new schemas  (optional).
+    def initialize(registry_url: nil, schemas_path: nil, namespace: nil, logger: nil, register: true)
       @logger = logger || Logger.new($stderr)
       @namespace = namespace
       @schema_store = SchemaStore.new(path: schemas_path || DEFAULT_SCHEMAS_PATH)
-      @registry = CachedSchemaRegistry.new(SchemaRegistry.new(registry_url, logger: @logger))
+      @registry = CachedSchemaRegistry.new(SchemaRegistry.new(registry_url, logger: @logger), register: register)
     end
 
     # Encodes a message using the specified schema.
@@ -37,15 +37,21 @@ class AvroTurf
     # schema_name - The String name of the schema that should be used to encode
     #               the data.
     # namespace   - The namespace of the schema (optional).
+    # version     - specifc a specific verision of a schema (optional).
     #
     # Returns the encoded data as a String.
-    def encode(message, schema_name: nil, namespace: @namespace)
-      schema = @schema_store.find(schema_name, namespace)
-
-      # Schemas are registered under the full name of the top level Avro record
-      # type.
-      schema_id = @registry.register(schema.fullname, schema)
-
+    def encode(message, schema_name: nil, namespace: @namespace, version: 'latest')
+      begin
+        schema = @schema_store.find(schema_name, namespace)
+        # Schemas are registered under the full name of the top level Avro
+        # record type.
+        schema_id = @registry.register(schema.fullname, schema)
+      rescue => e
+        @logger.info("Looking for schema: #{schema_name} at schema registry")
+        # Check rest proxy for it
+        schema_json = @registry.subject_version(schema_name, version)
+        schema, schema_id = register_found_schema(schema_json, schema_name)
+      end
       stream = StringIO.new
       writer = Avro::IO::DatumWriter.new(schema)
       encoder = Avro::IO::BinaryEncoder.new(stream)
@@ -55,7 +61,6 @@ class AvroTurf
 
       # The schema id is encoded as a 4-byte big-endian integer.
       encoder.write([schema_id].pack("N"))
-
       # The actual message comes last.
       writer.write(message, encoder)
 
@@ -90,6 +95,16 @@ class AvroTurf
 
       reader = Avro::IO::DatumReader.new(writers_schema, readers_schema)
       reader.read(decoder)
+    end
+
+    private
+
+    def register_found_schema(schema_json, schema_name)
+      fail 'No schema found' if schema_json.nil?
+      schema = Avro::Schema.parse(schema_json['schema'])
+      @schema_store.store!(schema.fullname, schema)
+      @registry.store!(schema.fullname, schema, schema_json['id'])
+      return schema, schema_json['id']
     end
   end
 end
