7ed98492d35d58c8154c6f42e8a4625110c728ed
Tim Perkins <tperkins@salsify.com>
2016-09-30 09:52:42 -0400

    Raise error for missing value for a field with no default

--- spec/schema_to_avro_patch_spec.rb
+++ spec/schema_to_avro_patch_spec.rb
@@ -22,3 +22,45 @@ describe Avro::Schema do
     })
   end
 end
+
+
+describe Avro::IO::DatumReader do
+  let(:writer_schema) do
+    Avro::Schema.parse <<-AVSC
+      {
+        "name": "no_default",
+        "type": "record",
+        "fields": [
+          { "type": "string", "name": "one" }
+        ]
+      }
+    AVSC
+  end
+  let(:reader_schema) do
+    Avro::Schema.parse <<-AVSC
+      {
+        "name": "no_default",
+        "type": "record",
+        "fields": [
+          { "type": "string", "name": "one" },
+          { "type": "string", "name": "two" }
+        ]
+      }
+    AVSC
+  end
+
+  it "raises an error for missing fields without a default" do
+    stream = StringIO.new
+    writer = Avro::IO::DatumWriter.new(writer_schema)
+    encoder = Avro::IO::BinaryEncoder.new(stream)
+    writer.write({ 'one' => 'first' }, encoder)
+    encoded = stream.string
+
+    stream = StringIO.new(encoded)
+    decoder = Avro::IO::BinaryDecoder.new(stream)
+    reader = Avro::IO::DatumReader.new(writer_schema, reader_schema)
+    expect do
+      reader.read(decoder)
+    end.to raise_error(Avro::AvroError, 'Missing data for "string" with no default')
+  end
+end
