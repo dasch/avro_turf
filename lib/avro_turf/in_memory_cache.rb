2bc1ab2270b10c78cd45626c4a765fe6d16b8c63
Baicheng Yu <baicheng.yu@qonto.eu>
2019-05-09 19:14:12 +0200

    Add in memory cache for schemas by subject

--- lib/avro_turf/in_memory_cache.rb
+++ lib/avro_turf/in_memory_cache.rb
@@ -5,6 +5,7 @@ class AvroTurf::InMemoryCache
   def initialize
     @schemas_by_id = {}
     @ids_by_schema = {}
+    @schemas_by_subject = {}
   end
 
   def lookup_by_id(id)
@@ -23,5 +24,13 @@ class AvroTurf::InMemoryCache
   def store_by_schema(subject, schema, id)
     key = subject + schema.to_s
     @ids_by_schema[key] = id
+  end
+
+  def lookup_by_subject(subject)
+    @schemas_by_subject[subject]
+  end
+
+  def store_by_subject(subject, schemas)
+    @schemas_by_subject[subject] = schemas
   end
 end
