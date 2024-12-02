e9676589513c1efa080c25e3909745497758a77c
Damián Serrano Thode <mail@dsthode.info>
2024-12-02 22:27:15 +0100

    Add fake servers for testing with host authorization

---
+++ spec/support/authorized_fake_confluent_schema_registry_server.rb
@@ -0,0 +1,5 @@
+require 'avro_turf/test/fake_confluent_schema_registry_server'
+
+class AuthorizedFakeConfluentSchemaRegistryServer < FakeConfluentSchemaRegistryServer
+  set :host_authorization, permitted_hosts: ['example.org', 'registry.example.com']
+end
---
+++ spec/support/authorized_fake_prefixed_confluent_schema_registry_server.rb
@@ -0,0 +1,5 @@
+require 'avro_turf/test/fake_prefixed_confluent_schema_registry_server'
+
+class AuthorizedFakePrefixedConfluentSchemaRegistryServer < FakePrefixedConfluentSchemaRegistryServer
+  set :host_authorization, permitted_hosts: ['example.org', 'registry.example.com']
+end
